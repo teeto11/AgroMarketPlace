2095acaeed58132a5c9ead579d06f969b4c033b3
teeto11 <elteeto11@gmail.com>
2020-05-21 07:08:59 +0100

    added welcome route for test purpose

--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@ const { port } = config;
 app.use(express.json())
 
 
-
+app.get('/', (req, res) => {
+    res.send('welcome');
+    console.log('started');
+ });
+ 
 app.listen(port,()=>{
     console.log(`${config. appName} started on port --> ${port}`)
 }) 
