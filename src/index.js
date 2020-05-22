5b6bf01c71bf6096b30af064ef7a96257c8b4af1
teeto11 <elteeto11@gmail.com>
2020-05-22 22:41:43 +0100

    adding dot.env to index

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ const config = require('./config/config');
 const app  = express();
 const marketRoutes = require('./routes/market')
 const adminRoutes = require('./routes/Admin')
+const dotenv = require('dotenv');
 require('./lib/dbconn')
+dotenv.config();
+
 const { PORT } = config;
 app.use(express.json())
 
